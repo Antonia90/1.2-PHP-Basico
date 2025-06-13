8f30be37a9794774bdca3f0a7bbaddb28f12bd93
Antonia90 <antominnucci@gmail.com>
2025-06-13 09:18:32 +0200

    Definición de las constantes fuera de la función

--- ejercicios_nivel_2/calculateTotalPaymentCall.php
+++ ejercicios_nivel_2/calculateTotalPaymentCall.php
@@ -1,11 +1,11 @@
 <?php
 
+const MIN_DURATION = 3;
+const MIN_COST = 10;
+const EXTRA_MIN_COST = 5;
+
 function calculateTotalPaymentCall(int|float $duration, bool $inCents = true): int|float
 {
-    define('MIN_DURATION', 3);
-    define('MIN_COST', 10);
-    define('EXTRA_MIN_COST', 5);
-
     if ($duration <= MIN_DURATION) {
         return MIN_COST;
     }
